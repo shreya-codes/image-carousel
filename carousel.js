a1cd92d8aac0fa81cbe32f271e4dbb74ca25a32c
Shreya Shrestha <shreya.shrestha1999@gmail.com>
2021-12-26 23:26:14 +0545

    next andd previous btn

--- carousel.js
+++ carousel.js
@@ -1,10 +1,11 @@
 const container= document.getElementsByClassName('carousel-container')
 const images= document.getElementById('images')
+const dots=document.getElementById('dots');
 
 const imageWidth=500;
 const imageCount=images.children.length;
 console.log(imageCount)
-
+console.log(dots.children[1])
 
 images.style.width=`${imageCount*imageWidth}px`;
 
@@ -20,27 +21,26 @@ let currentIndex=0;
 let interval;
 let slide=0;
 
-nextBtn.onclick= function(){
+function next(){
     currentIndex++ ;
+    slide=(currentIndex-1 )*500
     // images.style.left=`-${currentIndex*imageWidth}px`;
     interval= setInterval(()=>{
-        slide=slide+100;
+       
+            slide=slide+100;
         images.style.right=`${slide}px`
+        
         if (slide >=currentIndex*500){
         clearInterval(interval);
         }
 
         
         
-},10); 
-console.log(`slide${slide}`)
-console.log(`currentindex${currentIndex}`)
+},10);
 }
-console.log(`slide${slide}`)
-
-
-prevBtn.onclick= function(){
+function previous(){
     currentIndex-- ;
+    slide=(currentIndex-1)*500
     // images.style.left=`-${currentIndex*imageWidth}px`;
     interval= setInterval(()=>{
         slide=slide-100;
@@ -49,9 +49,69 @@ prevBtn.onclick= function(){
         clearInterval(interval);
         }
         
-},10); 
+},10);
+}
+
+dots.addEventListener('click', () => {
+    /**
+     * Runs every 17 milliseconds until the interval is cleared.
+     */
+    interval = setInterval(() => {
+        /**
+         * If clicked indicator dot is same as current pos, it clears the interval.
+         */
+        if (currentIndex === i) {
+            clearInterval(interval);
+        } else if (currentIndex > i) {
+            /**
+             * Runs if clicked indicator dot is before the current position.
+             */
+            distanceTravelled -= transitionSpeed;
+            carouselImageWrapper[0].style.left = -(distanceTravelled) + 'px';
+
+            if (distanceTravelled <= i * IMAGE_WIDTH) {
+                clearInterval(interval);
+                currentIndex = i;
+                setActivedots();
+            }
+        } else {
+            /**
+             * Runs if clicked indicator dot is after the current position.
+             */
+            distanceTravelled += transitionSpeed;
+            carouselImageWrapper[0].style.left = -(distanceTravelled) + 'px';
+            if (distanceTravelled >= i * IMAGE_WIDTH) {
+                clearInterval(interval);
+                currentIndex = i;
+                setActivedots();
+            }
+        }
+    }, 17);
+});
+
+
+nextBtn.onclick= function(){
+  if(currentIndex!=imageCount-1){
+      next();
+      console.log(`slide111 ${slide} current ${currentIndex}`)
+  }
+  else{
+      previous();
+      console.log(`slide ${slide} current ${currentIndex}`)
+  }
+}
 console.log(`slide${slide}`)
-console.log(`currentindex${currentIndex}`)
+
+
+prevBtn.onclick= function(){
+   if(currentIndex>0){
+       previous();
+       console.log(`slide ${slide} current ${currentIndex}`)
+   }
+   else{
+       next()
+       console.log(`slide ${slide} current ${currentIndex}`)
+   }
 }
 console.log(`slide${slide}`)
 
