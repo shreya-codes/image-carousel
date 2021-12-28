95401d33cd017b23c3ae4aa8f36ab779d2a71417
Shreya Shrestha <shreya.shrestha1999@gmail.com>
2021-12-28 11:56:10 +0545

    dots indicator complete

--- carousel.js
+++ carousel.js
@@ -4,8 +4,7 @@ const dots=document.getElementById('dots');
 
 const imageWidth=500;
 const imageCount=images.children.length;
-console.log(imageCount)
-console.log(dots.children[1])
+
 
 images.style.width=`${imageCount*imageWidth}px`;
 
@@ -13,106 +12,109 @@ for (let i =0;i < imageCount;i++){
     const image=images.children[i];
     image.style.left=`${i*imageWidth}px`
 }
-console.log(images.children[2])
 
 const nextBtn=document.getElementById('next');
 const prevBtn= document.getElementById('prev')
 let currentIndex=0;
 let interval;
 let slide=0;
+let marginPx=0
+
+nextBtn.onclick= function(){next()}
+prevBtn.onclick= function(){ previous()}
 
 function next(){
-    currentIndex++ ;
-    slide=(currentIndex-1 )*500
-    // images.style.left=`-${currentIndex*imageWidth}px`;
-    interval= setInterval(()=>{
-       
-            slide=slide+100;
-        images.style.right=`${slide}px`
-        
-        if (slide >=currentIndex*500){
-        clearInterval(interval);
-        }
-
-        
-        
-},10);
+    
+    if (currentIndex==imageCount-1){
+        currentIndex=0
+        slideToPrev(0,30);
+    }
+    else{
+        currentIndex++;
+        marginPx=-(500*currentIndex)
+        slideToNext(marginPx,30);
+    }
+    setActivedots(currentIndex)
 }
+
 function previous(){
-    currentIndex-- ;
-    slide=(currentIndex-1)*500
-    // images.style.left=`-${currentIndex*imageWidth}px`;
-    interval= setInterval(()=>{
-        slide=slide-100;
-        images.style.right=`${slide}px`
-        if (slide <=currentIndex*500){
-        clearInterval(interval);
-        }
-        
-},10);
-}
-
-dots.addEventListener('click', () => {
-    /**
-     * Runs every 17 milliseconds until the interval is cleared.
-     */
-    interval = setInterval(() => {
-        /**
-         * If clicked indicator dot is same as current pos, it clears the interval.
-         */
-        if (currentIndex === i) {
-            clearInterval(interval);
-        } else if (currentIndex > i) {
-            /**
-             * Runs if clicked indicator dot is before the current position.
-             */
-            distanceTravelled -= transitionSpeed;
-            carouselImageWrapper[0].style.left = -(distanceTravelled) + 'px';
-
-            if (distanceTravelled <= i * IMAGE_WIDTH) {
-                clearInterval(interval);
-                currentIndex = i;
-                setActivedots();
-            }
-        } else {
-            /**
-             * Runs if clicked indicator dot is after the current position.
-             */
-            distanceTravelled += transitionSpeed;
-            carouselImageWrapper[0].style.left = -(distanceTravelled) + 'px';
-            if (distanceTravelled >= i * IMAGE_WIDTH) {
-                clearInterval(interval);
-                currentIndex = i;
-                setActivedots();
-            }
-        }
-    }, 17);
-});
-
-
-nextBtn.onclick= function(){
-  if(currentIndex!=imageCount-1){
-      next();
-      console.log(`slide111 ${slide} current ${currentIndex}`)
-  }
-  else{
-      previous();
-      console.log(`slide ${slide} current ${currentIndex}`)
-  }
-}
-console.log(`slide${slide}`)
-
-
-prevBtn.onclick= function(){
-   if(currentIndex>0){
-       previous();
-       console.log(`slide ${slide} current ${currentIndex}`)
+   if(currentIndex!==0){
+       var slidePx=marginPx+500;
+       slideToPrev(slidePx,30);
+       currentIndex--;
    }
    else{
-       next()
-       console.log(`slide ${slide} current ${currentIndex}`)
+       currentIndex=imageCount-1;//2
+       slidePx=-(500*currentIndex)
+       slideToNext(slidePx,30)
+       
    }
+
+   setActivedots(currentIndex);
 }
-console.log(`slide${slide}`)
+
+function slideToNext(marginPx,slidePx){
+    var interval=setInterval(()=>{
+        slide-=slidePx
+        images.style.left=`${slide}px`;
+
+    },10)
+    setInterval(()=>{
+        if(slide>marginPx){
+            interval
+        }
+        else{
+            clearInterval(interval)
+        }
+
+    },10)
+
+
+}
+function slideToPrev(marginPx,slidePx){
+    var interval=setInterval(()=>{
+        slide+=slidePx
+        images.style.left=`${slide}px`;
+
+    },10)
+    setInterval(()=>{
+        if(slide < marginPx){
+            interval
+        }
+        else{
+            clearInterval(interval)
+        }
+
+    },10)
+
+}
+function setActivedots(currentIndex){
+    for(var i=0 ; i<imageCount;i++){
+        dots.children[i].classList.remove('active');
+    }
+    dots.children[currentIndex].setAttribute('class','active'
+    )
+}
+
+for (let i = 0, len = dots.children.length; i < len; i++)
+{
+    dots.children[i].onclick = function(){
+       
+            if (currentIndex === i) {
+                clearInterval(interval);
+            }
+            else if (currentIndex > i) {
+               
+               previous()
+
+            }
+            else {
+                
+                next()
+                }
+            
+      
+    }
+}
 
 
